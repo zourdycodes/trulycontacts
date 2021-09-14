db4bd7a7bda096c7ee3682a8f5625925efebf350
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-14 19:10:12 +0700

    :art: setting up the folder structures
    checked

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import logo from "./assets/images/logo.svg";
+import "./app.css";
+
+export const App = () => {
+  return (
+    <div className="app">
+      <h1>hello world</h1>
+    </div>
+  );
+};
